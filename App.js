febbde0c2c33f5c8c780914772471ed614e4d557
Савин Георгий Викторович <GVSavin.SBT@sberbank.ru>
2021-10-08 23:36:50 +0300

    свайпер - начал переделывать под свайпер

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import Navigation from './src/navigation';
+import CardContiner from './src/components/CardContiner.js';
 
 export default function App() {
   return (
-    <Navigation />
+    <CardContiner />
   );
 }
 

9671146a9664daefc629c0c459dec9d5db12529a
Denis Malin <denis@malina.page>
2021-10-09 13:12:55 +0300

    refactoring

--- src/screens/otherScreen/VideoScreen.js
+++ src/screens/otherScreen/VideoScreen.js
@@ -1,38 +1,31 @@
 import React from 'react';
 import {
-  Button,
-  StyleSheet, View,
+  StyleSheet,
 } from 'react-native';
 
-import { Colors } from '../../styles';
-import { Video, AVPlaybackStatus } from 'expo-av';
+import { Video } from 'expo-av';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: Colors.blue,
   },
   backgroungImage: {
     width: '100%',
     height: '100%',
-    backgroundColor: Colors.blue,
   },
 });
 
-const Url = "http://video.hakaton.website.yandexcloud.net/"
-
-const VideoScreen = () => {
+const VideoScreen = ({ url }) => {
   const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
   return (
     <Video
       ref={video}
       style={styles.backgroungImage}
       source={{
-        uri: `${Url}v16-web.tiktok.com.mp4`,
+        uri: url,
       }}
-      // useNativeControls
+      useNativeControls
       resizeMode="contain"
       isLooping
     />

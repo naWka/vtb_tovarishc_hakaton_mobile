1e3bab8b4ae13a0ff6fc55c6af1bed999cd8b52f
Pavel Shestakov <pshestakov94@gmail.com>
2021-10-10 12:27:53 +0700

    fixed AgeCard

--- src/components/AgeCard.jsx
+++ src/components/AgeCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {
-  Text, View, StyleSheet, TouchableOpacity,
+  Text, View, StyleSheet, TouchableOpacity, Image,
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign } from '@expo/vector-icons';
+import end from '../assets/images/ageend.jpeg';
+import sword from '../assets/images/sword.png';
+import myBack from '../assets/images/myBack.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,13 +20,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(20, 49, 92, 1)',
     borderRadius: 12,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     padding: 16,
   },
   cardText: {
     color: 'white',
     fontSize: 25,
     fontWeight: 'bold',
+    marginTop: 70,
   },
   likesContainer: {
     flexDirection: 'row',
@@ -34,23 +38,29 @@ const styles = StyleSheet.create({
   },
 });
 
-const cards = [
-  {
-    text: 'Думаю, что на зубы нужно было начинать откладывать еще с карманных денег в школьные годы',
-  },
-  {
-    text: 'Настоящий джедай знает истинное предназначение этого меча',
-  },
-  {
-    text: 'Что нужно моей спине',
-  },
-];
 const AgeTest = ({ onLike }) => {
+  const cards = [
+    {
+      text: 'Думаю, что на зубы нужно было начинать откладывать еще с карманных денег в школьные годы',
+    },
+    {
+      text: 'Настоящий джедай знает истинное предназначение этого меча',
+      image: sword,
+      iamgeStyle: { width: '100%', height: 50 },
+    },
+    {
+      iamgeStyle: { width: '100%', height: 250 },
+      text: 'Что нужно моей спине',
+      image: myBack,
+    },
+  ];
+
   const [activeCard, setActiveCard] = React.useState(0);
   const [score, setScore] = React.useState(20);
 
   const renderCard = () => (
     <View style={styles.card}>
+      {cards[activeCard].image && <Image source={cards[activeCard].image} style={cards[activeCard].iamgeStyle} resizeMode="contain" />}
       <Text style={styles.cardText}>{cards[activeCard].text}</Text>
     </View>
   );
@@ -78,7 +88,7 @@ const AgeTest = ({ onLike }) => {
       colors={['rgba(176, 205, 249, 1)', 'rgba(58, 131, 241, 1)', 'rgba(34, 80, 148, 1)']}
       style={styles.container}
     >
-      {!isEnd && renderCard()}
+      {!isEnd ? renderCard() : <Image source={end} style={{ width: 250, height: 250 }} />}
       {!isEnd && (
       <View style={styles.likesContainer}>
         <TouchableOpacity onPress={hendleDislikePress}>

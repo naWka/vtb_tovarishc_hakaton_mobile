1bb28c1279b77b6a0a5852446028866fa32b7fa7
Pavel Shestakov <pshestakov94@gmail.com>
2021-10-10 12:17:11 +0700

    fixed menu

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Swipe from '../screens/swipe/Swipe';
-import MenuScreen from '../screens/entryScreen/MenuScreen';
+// import MenuScreen from '../screens/entryScreen/MenuScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,7 +19,7 @@ export default function Navigation() {
             headerShown: false,
           }}
         />
-        <Stack.Screen name="Menu" component={MenuScreen} />
+        {/* <Stack.Screen name="Menu" component={MenuScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
